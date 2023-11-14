aa335040386d7658b0eb243cc2f6b7f1593a0a9e
JoseENarvaezM <josenarvaezm2003@gmail.com>
2023-11-14 15:09:13 -0500

    navbar

--- src/components/NavigationItem.jsx
+++ src/components/NavigationItem.jsx
@@ -1,38 +1,70 @@
-import './styles/NavigationItem.css'
+// Importaciones
+import './styles/NavigationItem.css';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faCalendarPlus, faList, faPencilAlt, faListAlt, faCheck, faBars } from '@fortawesome/free-solid-svg-icons';
 
-function NavigationItem() {
-    return (
-        <nav className="navbar navbar-expand-custom navbar-mainbg">
-            <button className="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i className="fas fa-bars text-white"></i>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
-                    <div className="hori-selector"><div className="left"></div><div className="right"></div></div>
-                    <li className="nav-item">
-                        <a className="nav-link" href="javascript:void(0);"><i className="fas fa-tachometer-alt"></i>Main Page</a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="javascript:void(0);"><i className="far fa-address-book"></i>Create Academic Period</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="javascript:void(0);"><i className="far fa-clone"></i>Academic Period List</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="javascript:void(0);"><i className="far fa-calendar-alt"></i>Create Labor</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="javascript:void(0);"><i className="far fa-chart-bar"></i>Labor List</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="javascript:void(0);"><i className="far fa-copy"></i>Self Evaluation</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="javascript:void(0);"><i className="far fa-copy"></i>Self Evaluation List</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+const NavigationItem = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  return (
+    <div id="navbar">
+      <div className="icono-hamburguesa" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+
+      <a href="#" id="pagina-principal">
+        <FontAwesomeIcon icon={faHome} /> Página Principal
+      </a>
+      <a href="#">
+        <FontAwesomeIcon icon={faCalendarPlus} /> Crear Periodo Académico
+      </a>
+      <a href="#">
+        <FontAwesomeIcon icon={faList} /> Lista De Períodos Académicos
+      </a>
+      <a href="#">
+        <FontAwesomeIcon icon={faPencilAlt} /> Crear Labor
+      </a>
+      <a href="#">
+        <FontAwesomeIcon icon={faListAlt} /> Lista de Labores
+      </a>
+      <a href="#">
+        <FontAwesomeIcon icon={faCheck} /> Autoevaluación
+      </a>
+      <a href="#">
+        <FontAwesomeIcon icon={faListAlt} /> Lista De Autoevaluaciones
+      </a>
+
+      {/* Menú desplegable */}
+      <div className={`menu-desplegable ${menuVisible ? 'visible' : ''}`}>
+        <a href="#" id="pagina-principal-movil">
+          <FontAwesomeIcon icon={faHome} /> Página Principal
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faCalendarPlus} /> Crear Periodo Académico
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faList} /> Lista De Períodos Académicos
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faPencilAlt} /> Crear Labor
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faListAlt} /> Lista de Labores
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faCheck} /> Autoevaluación
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faListAlt} /> Lista De Autoevaluaciones
+        </a>
+      </div>
+    </div>
+  );
+};
+
 export default NavigationItem;
