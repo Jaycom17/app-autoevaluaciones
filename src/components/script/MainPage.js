ce0c7d9683ec7494f2210a850d4243555e9f978c
Jaycom17 <juanorejuela499@gmail.com>
2023-11-26 10:59:46 -0500

    done

--- src/components/script/MainPage.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var boton = document.getElementById("mostrarBoton");
-    var imagen = document.getElementById("miImagen");
-
-    boton.addEventListener("click", function() {
-        imagen.style.display = "block";
-    });
-});
